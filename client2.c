0427d24d584fecc63e8df781fa3ebcdecf014f57
Eetu Kemppi <eetu.kemppi@protonmail.com>
2024-04-13 16:43:28 +0300

    React to changes in output3 values

--- client2.c
+++ client2.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
 #define PORT_OUT1 4001
 #define PORT_OUT2 4002
@@ -41,6 +42,17 @@ void print_output(long long timestamp_ms, char *outputs[]) {
                             timestamp_ms, outputs[0], outputs[1], outputs[2]);
 }
 
+// Only react when the output3 crosses the limit
+void process_output3(float val, bool *isOverLimit) {
+    if (*isOverLimit == false && val >= 3.0) {
+        printf("output3 went over 3\n");
+        *isOverLimit = true;
+    } else if (*isOverLimit == true && val < 3.0) {
+        printf("output3 went under 3\n");
+        *isOverLimit = false;
+    }
+}
+
 int main() {
     int sockets[NUM_SOCKETS] = {create_socket(PORT_OUT1), create_socket(PORT_OUT2), create_socket(PORT_OUT3)};
     int maxfd = 0;
@@ -60,6 +72,8 @@ int main() {
     timeout.tv_usec = 20000;
     struct timeval now;
 
+    bool isOverLimit;   // 3.0 is the limit to act upon in output3
+
     struct timeval last_print_time;
     gettimeofday(&last_print_time, NULL);
     while (1) {
@@ -68,7 +82,7 @@ int main() {
             FD_SET(sockets[i], &readfds);
         }
 
-        // Monitor multiple sockets simultaneously (timeout at 100ms)
+        // Monitor multiple sockets simultaneously (timeout at 20ms)
         int activity = select(maxfd, &readfds, NULL, NULL, &timeout);
 
         if (activity < 0) {
@@ -94,12 +108,17 @@ int main() {
                         char val_str[16];
                         snprintf(val_str, sizeof(val_str), "%0.1f", out_value);
                         outputs[i] = strdup(val_str);
+
+                        // i=2 means output3 socket
+                        if (i == 2) {
+                            process_output3(out_value, &isOverLimit);
+                        }
                     }
                 }
             }
         }
 
-        // Check if 100ms has elapsed. If yes, print and reset the stored outputs
+        // Check if 20ms has elapsed. If yes, print and reset the stored outputs
         gettimeofday(&now, NULL);
         long long now_ms = now.tv_sec * 1000LL + now.tv_usec / 1000;
         if (now_ms - (last_print_time.tv_sec * 1000LL + last_print_time.tv_usec / 1000) >= 20) {
